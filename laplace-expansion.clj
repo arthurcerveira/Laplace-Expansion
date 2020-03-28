12d89f173d13ddc11ac2922c0aab47a22dcbca11
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-03-28 12:16:46 -0300

    Implement get-minor-matrix and laplace-expansion

--- laplace-expansion.clj
+++ laplace-expansion.clj
@@ -2,26 +2,51 @@
                     [3 0 5]
                     [-1 9 11]))
 
-(defn determinant [m]
-  (def a (get (get m 0) 0))
-  (def b (get (get m 0) 1))
-  (def c (get (get m 1) 0))
-  (def d (get (get m 1) 1))
+(defn determinant [matrix]
+  (def a (get (get matrix 0) 0))
+  (def b (get (get matrix 0) 1))
+  (def c (get (get matrix 1) 0))
+  (def d (get (get matrix 1) 1))
 
   (- (* a d) (* b c)))
 
-(defn laplace-expansion [m]
-  (def matrix-lenght (count m))
-  (def index 0)
+(defn get-minor-matrix [matrix, index]
+  ; Remove the top row
+  (def minor-matrix (into (vector) (remove #(= (get matrix 0) %) matrix)))
+
+  ; Remove the column determined by the index
+  (def minor-matrix
+    (mapv (fn [row]
+            (remove #(= (get row index) %) row)) minor-matrix))
+
+  ; Turns seq into vector
+  (mapv (fn [row] (into [] row)) minor-matrix))
+
+(defn exp [x n]
+  (if (zero? n) 1
+      (* x (exp x (dec n)))))
+
+(defn laplace-expansion [matrix]
+  (def matrix-lenght (count matrix))
+
+  ; Map the index to the array so they can be accessed by reduce
+  (def mapped-array (zipmap (get matrix 0) (range matrix-lenght)))
 
   (if (= matrix-lenght 2)
     ; Base case of the recursion
-    (determinant m)
-    (reduce (fn [result number]
-              (println "index:" index)
-              (println "result:" result)
-              (println "number:" number)
-              (def index (+ index 1))
-              (+ number result)) 0 (get m 0))))
+    (determinant matrix)
+    ; Else
+    (reduce-kv
+     (fn [result number index]
+       (def minor-matrix (get-minor-matrix matrix index))
+
+       ; Induction step of the recursion
+       (def minor (* number (laplace-expansion minor-matrix)))
+
+       (def sign (exp -1 index))
+       (def cofactor (* sign minor))
+
+       (def index (+ index 1))
+       (+ cofactor result)) 0 mapped-array)))
 
 (println (laplace-expansion matrix))
